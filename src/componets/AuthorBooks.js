4e7bb373d783b17bae3f25657f89df44baef5e59
Vitalii Speka <spekava@gmail.com>
2021-02-06 13:29:48 +0200

    +

---
+++ src/componets/AuthorBooks.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AuthorBooks extends Component {
+  render() {
+    return <h1>Компонет книг автора 1</h1>;
+  }
+}
+
+export default AuthorBooks;
--- src/views/AuthorsView.js
+++ src/views/AuthorsView.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import { Link, Route } from "react-router-dom";
+import "../index.css";
+import AuthorBooks from "../componets/AuthorBooks";
 
 export class AuthorsView extends Component {
   state = {
@@ -14,14 +17,26 @@ export class AuthorsView extends Component {
   }
 
   render() {
+    console.log(this.props.match.url);
+    const { match } = this.props;
+
     return (
       <>
         <h1>Это страница Авторов</h1>;
         <ul>
           {this.state.authors.map(({ id, name }) => (
-            <li key={id}>{name}</li>
+            <li key={id}>
+              <Link to={`${match.url}/${id}`}>{name}</Link>
+            </li>
           ))}
         </ul>
+        <Route
+          path={`${match.path}/:authorId`}
+          component={AuthorBooks}
+          render={(props) => (
+            <AuthorBooks {...props} authors={this.state.authors} />
+          )}
+        />
       </>
     );
   }
