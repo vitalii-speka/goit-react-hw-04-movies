c0fd4d7f214764a8a74c0bd1347b0fb12c550631
Vitalii Speka <spekava@gmail.com>
2021-02-09 02:47:35 +0200

    Netlify!

--- src/views/MoviesPage/MoviesPage.js
+++ src/views/MoviesPage/MoviesPage.js
@@ -4,6 +4,7 @@ import Axios from "axios";
 import MovieList from "../../componets/MovieList";
 import Searchbar from "../../componets/Searchbar";
 import getQueryPatams from "../../utils/getQueryPatams";
+import { toast } from "react-toastify";
 
 export class MoviesPage extends Component {
   state = {
@@ -12,7 +13,7 @@ export class MoviesPage extends Component {
 
   async componentDidMount() {
     const { query } = getQueryPatams(this.props.location.search);
-
+    console.log(query);
     if (query) {
       const response = await Axios.get(
         `https://api.themoviedb.org/3/search/movie?api_key=66851c2d78ce86a1843cb2ac55e2da92&language=en-US&query=${query}&page=1&include_adult=false`
@@ -33,6 +34,10 @@ export class MoviesPage extends Component {
         `https://api.themoviedb.org/3/search/movie?api_key=66851c2d78ce86a1843cb2ac55e2da92&language=en-US&query=${nextQuery}&page=1&include_adult=false`
       );
 
+      if (response.data.results.length === 0) {
+        return toast.info("please, enter your request");
+      }
+
       this.setState({ movies: response.data.results });
     }
   }
@@ -49,6 +54,7 @@ export class MoviesPage extends Component {
     return (
       <>
         <Searchbar onSubmit={this.handleChangeQuery} />
+
         <MovieList movies={this.state.movies} />
       </>
     );
